abaa9ef864c24e528d9a373b7cbdc9b50c6a775e
Vamato <62358492+Vamato2008@users.noreply.github.com>
2022-02-08 19:06:32 +0000

    Added solution 2

--- Easy/p9.py
+++ Easy/p9.py
@@ -1,3 +1,10 @@
+#Solution 1
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return False if str(x)[::-1] != str(x) else True
+    
+#Solution 2
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        return s==s[::-1]
