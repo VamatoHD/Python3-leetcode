eb13702479b892683306b01823a0d9e4332c440c
Vamato <62358492+Vamato2008@users.noreply.github.com>
2022-02-12 20:03:15 +0000

    Modified p37.py

--- Hard/p37.py
+++ Hard/p37.py
@@ -1,16 +1,13 @@
+#Bad Solution 1
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
         def solve(bo):
             find = find_empty(bo)
             if not find:
                 return True
             else:
                 row, col = find
-
-            for i in range(1, 10):
+            for i in range(1,10):
                 if valid(bo, str(i), (row, col)):
                     bo[row][col] = str(i)
 
@@ -26,43 +23,23 @@ class Solution:
             for i in range(len(bo[0])):
                 if bo[pos[0]][i] == num and pos[1] != str(i):
                     return False
-
             # Check column
             for i in range(len(bo)):
                 if bo[i][pos[1]] == num and pos[0] != str(i):
                     return False
-
             # Check box
             box_x = pos[1] // 3
             box_y = pos[0] // 3
-
             for i in range(box_y*3, box_y*3 + 3):
                 for j in range(box_x * 3, box_x*3 + 3):
-                    if bo[i][j] == num and (i, j) != pos:
+                    if bo[i][j] == num and (i,j) != pos:
                         return False
-
             return True
-
-        def print_board(bo):
-            for i in range(len(bo)):
-                if i % 3 == 0 and i != 0:
-                    print("- - - - - - - - - - - - - ")
-
-                for j in range(len(bo[0])):
-                    if j % 3 == 0 and j != 0:
-                        print(" | ", end="")
-
-                    if j == 8:
-                        print(bo[i][j])
-                    else:
-                        print(str(bo[i][j]) + " ", end="")
-
+        
         def find_empty(bo):
             for i in range(len(bo)):
                 for j in range(len(bo[0])):
                     if bo[i][j] == ".":
                         return (i, j)  # row, col
-
             return None
-
         solve(board)
